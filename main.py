eb0f0eff87b8df337c26be6d027ca303d988d8b2
Gleb Klyga <95571880+Gleb76@users.noreply.github.com>
2022-04-11 13:37:12 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+def bracket_sequence(sequence):
+    stack = []
+    brackets = {')': '(', ']': '[', '}': '{'}
+    first_left_bracket_pos = 0
+
+    for i, char in enumerate(sequence, 1):
+        if char in brackets.values():
+            stack.append(char)
+            if first_left_bracket_pos == 0:
+                first_left_bracket_pos = i
+
+        elif char in brackets:
+            if not stack or brackets[char] != stack.pop():
+                return i
+
+            if not stack:
+                first_left_bracket_pos = 0
+
+    return first_left_bracket_pos if stack else 'Success'
+
+
+def main():
+    sequence = input()
+    print(bracket_sequence(sequence))
+
+
+if __name__ == '__main__':
+    main()
